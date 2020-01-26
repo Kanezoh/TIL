b96d0f26a20e0a7a65284a995405c2f27b02e11e
Kanezoh <asfaltmanta114@gmail.com>
2020-01-26 11:26:57 +0900

    Continue

--- haskell/chapter-3/code.hs
+++ haskell/chapter-3/code.hs
@@ -35,3 +35,30 @@ overwrite x = (\x ->
                   (\x -> x) 4
                 )3
               )2
+-- スコープによる変数参照の違い
+x = 4
+-- 引数+4を返す
+add1 y = y + x
+
+-- 引数+3を返す(上で定義した変数xは関係なし)
+add2 y = (\x -> y + x)3
+
+-- 常に3を返す(引数も上で定義した変数も参照していない)
+add3 y = (\y ->
+          (\x -> y + x) 1) 2
+
+
+-- 3.6 練習問題  
+-- 3-1 この章で作った各関数をラムダ関数化してみる
+-- 1. 2倍にする関数
+--(\x -> x * 2)
+--2. sumSquareOrSquareSum関数
+(\x y ->
+  let sumSquare = (x+y)^2
+      squareSum = (x^2) + (y^2)
+      in
+       if sumSquare > squareSum
+       then sumSquare
+       else squareSum
+)
+
