8ca8c67cedb1891a0318cbc49a94c603cfae2fdd
Kanezoh <asfaltmanta114@gmail.com>
2020-07-21 21:50:40 +0900

    アクションを学習

--- vue/chapter-6/sample_vue.js
+++ vue/chapter-6/sample_vue.js
@@ -3,6 +3,28 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+function getCountNum(type) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      let amount
+      switch (type) {
+        case 'one':
+          amount = 1
+          break
+        case 'two':
+          amount = 2
+          break
+        case 'ten':
+          amount = 10
+          break
+        default:
+          amount = 0
+      }
+      resolve({ amount })
+    },1000)
+  })
+}
+
 const store = new Vuex.Store({
   state: {
     count: 0
@@ -15,9 +37,24 @@ const store = new Vuex.Store({
     increment(state, amount) {
       state.count += amount
     }
+  },
+  actions: {
+    incrementAsync({ commit }, payload) {
+      return getCountNum(payload.type)
+        .then(data => {
+          console.log(data)
+          commit('increment', {
+            amount: data.amount
+          })
+        })
+    }
   }
 })
 
 console.log(store.state.count) // 0
 store.commit('increment', 1)
 console.log(store.state.count)
+
+console.log(store.getters.cubed)
+
+store.dispatch('incrementAsync', { type: 'one' })
