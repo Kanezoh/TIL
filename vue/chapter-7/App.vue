6726b9203e9bb3c72bba0808948a6b3046c2c0ab
Kanezoh <asfaltmanta114@gmail.com>
2020-07-24 16:26:56 +0900

    ローカルストレージにデータを保存

--- vue/chapter-7/App.vue
+++ vue/chapter-7/App.vue
@@ -40,6 +40,10 @@
          フィルタしない
       </li>
     </ul>
+
+    <h2>保存と復元</h2>
+    <button type="button" v-on:click="save">保存</button>
+    <button type="button" v-on:click="restore">復元</button>
   </div>
 </template>
 
@@ -92,6 +96,12 @@ export default {
       this.$store.commit('changeFilter', {
         filter: labelId
       })
+    },
+    save() {
+      this.$store.dispatch('save')
+    },
+    restore() {
+      this.$store.dispatch('restore')
     }
   }
 }
