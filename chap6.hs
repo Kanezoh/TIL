44957cb1bfcaf958f7c309e09c7b29bc7ba1d4c6
Kanezoh <asfaltmanta114@gmail.com>
2020-09-10 07:01:52 +0900

    リストの関数を学習

--- chap6.hs
+++ chap6.hs
@@ -44,3 +44,21 @@ main = do
   print (reverse[1,2,3])
   print (reverse("cheese"))
 
+  --elem関数 値とリストを引数にとってその値がリストに含まれているかどうかを判定する
+  print (elem 13 [0,13 .. 100])
+  print (elem 'p' "cheese")
+
+  --take関数 数値とリストを引数にとってその数値分リストから切り出す
+  print (take 5 [2,4 .. 100])
+  print (take 3 "wonderful")
+  --リストに含まれている要素の数より数値が大きい場合はできる限り切り出す
+  print (take 10000 [1])
+
+  --drop関数 先頭から指定された要素数を削除する
+  print (drop 2 [1,2,3,4,5])
+  print (drop 5 "very awesome")
+
+  --zip関数 2つのリストを組み合わせてタプルのペアを作成できる
+  print (zip [1,2,3] [4,5,6])
+  print (zip "dog" "rabbit")
+  print (zip ['a' .. 'f'] [1 .. ])
