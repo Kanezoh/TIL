209f051cf6592fd9e71ecafdc2b16368184ea72e
Kanezoh <asfaltmanta114@gmail.com>
2020-09-12 06:46:35 +0900

    パターンマッチングを実装

--- chap7.hs
+++ chap7.hs
@@ -9,3 +9,33 @@ module Main where
             then b
             else myGcd b remainder
     where remainder = a `mod` b
+  -- パターンマッチング
+  sayAmount n = case n of
+    1 -> "one"
+    2 -> "two"
+    n -> "a bunch"
+  
+  sayAmount 1 = "one"
+  sayAmount 2 = "two"
+  sayAmount 3 = "a bunch"
+
+  isEmpty [] = True
+  isEmpty _ = False --アンダーバーは使用しない値を示すワイルドカード
+  
+  -- head関数の再実装
+  myHead (x:xs) = x
+  myHead [] = error "No head for empty list"
+
+  -- tail関数の再実装
+  myTail (x:xs) = xs
+  myTail [] = error "No tail for empty list"
+
+  myGcdPattenMatch a 0 = a
+  myGcdPattenMatch a b = myGcdPattenMatch b (a `mod` b)
+
+  main = do
+    print (myHead [1,2,3])
+
+
+
+
