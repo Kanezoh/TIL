05e7940a79cd0ae60ed640bff8bec024decc6fbe
Kanezoh <asfaltmanta114@gmail.com>
2020-09-13 13:39:17 +0900

    再帰をさらに深く

---
+++ chap8.hs
@@ -0,0 +1,25 @@
+module Main where
+  import Lib
+  import Data.List
+
+  main::IO()
+
+  myLength [] = 0
+  myLength xs = 1 + myLength (tail xs)
+
+  -- パターンマッチングによるmyLengthの実装(tailを使用しない)
+  -- myLength [] = 0
+  -- myLength (x:xs) = 1 + myLength xs
+
+  -- takeの再実装
+  myTake _ [] = []
+  myTake 0 _ = []
+  myTake n (x:xs) = x:rest
+    where rest = myTake (n - 1) xs
+  
+  finiteCycle (first:rest) = first:rest ++ [first]
+  myCycle (first:rest) = (first:rest)++myCycle (first:rest)
+  
+
+  main = do 
+    print "ababa"
