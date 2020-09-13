d7b17c1de312b8b4af49e049e164f6f23c46fc19
Kanezoh <asfaltmanta114@gmail.com>
2020-09-13 15:12:02 +0900

    様々な再帰関数を学習

--- chap8.hs
+++ chap8.hs
@@ -18,12 +18,29 @@ module Main where
     where rest = myTake (n - 1) xs
   
   finiteCycle (first:rest) = first:rest ++ [first]
+  -- cycle関数の実装
   myCycle (first:rest) = (first:rest)++myCycle (first:rest)
 
+  -- アッカーマン関数の実装
   ackermann 0 n = n + 1
   ackermann m 0 = ackermann (m - 1) 1
   ackermann m n = ackermann (m-1) (ackermann m (n-1))
-  
 
+  -- コラッツ予想、100京くらいまで成立するらしい
+  collatz 1 = 1
+  collatz n = if even n
+              then collatz(n `div` 2)
+              else collatz(3 * n + 1)
+  
+  -- reverse関数の実装
+  myReverse [] = []
+  myReverse (x:[]) = [x]
+  myReverse (x:xs) = (myReverse xs) ++ [x]
+
+  -- フィボナッチ関数
+  fib 0 = 0
+  fib 1 = 1
+  fib n = fib (n - 1) + fib(n - 2)
+  
   main = do 
     print "ababa"
