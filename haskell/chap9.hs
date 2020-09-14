aa20d0cf7c7b1758dc9353d4b7519664fc7eba43
Kanezoh <asfaltmanta114@gmail.com>
2020-09-14 12:29:53 +0900

    filter関数を学習

--- haskell/chap9.hs
+++ haskell/chap9.hs
@@ -14,7 +14,15 @@ module Main where
   -- mapに渡す再帰関数を抽象化するとこうなる、つまりmapそのものの定義と同じ
   myMap f [] = []
   myMap f (x:xs) = (f x):myMap f xs
-  -- map関数、リストの各要素に対して渡された関数を実行
-  main = do
-    print(map reverse ["dog", "cat", "moose"])
 
+  -- remove、テストにパスした要素を削除
+  remove f [] = []
+  remove f (x:xs) = if f x
+                    then remove f xs
+                    else x:remove f xs
+  main = do
+    -- map関数、リストの各要素に対して渡された関数を実行
+    print(map reverse ["dog", "cat", "moose"])
+    -- filter関数、Trueになった要素だけを残す関数
+    print(filter even [1,2,3,4,5])
+    print(filter (\(x:xs) -> x == 'a')["apple", "banana", "avocado"])
