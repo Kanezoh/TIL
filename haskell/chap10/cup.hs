d10ee1460104c642170a534e52d21c95c0265dd3
Kanezoh <asfaltmanta114@gmail.com>
2020-09-16 00:06:43 +0900

    アクセサを定義

--- haskell/chap10/cup.hs
+++ haskell/chap10/cup.hs
@@ -1,10 +1,13 @@
 module Main where
   
-  cup f10z = \message -> message f10z
-
   -- コンストラクタ
-  let aCup = cup 6
+  cup f10z = \message -> message f10z
+  -- アクセサ
+  getOz aCup = aCup (\f10z -> f10z)
+  --let aCup = cup 6
   
   main = do
-    
+    print "ababa"
+    print "ababa"
+
 
