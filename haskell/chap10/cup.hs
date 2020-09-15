8b7e9e153301e1a858270a364b8492aed043a331
Kanezoh <asfaltmanta114@gmail.com>
2020-09-16 07:05:44 +0900

    コーヒーのメソッド作成

--- haskell/chap10/cup.hs
+++ haskell/chap10/cup.hs
@@ -5,6 +5,20 @@ module Main where
   -- アクセサ
   getOz aCup = aCup (\f10z -> f10z)
   --let aCup = cup 6
+
+  -- コーヒーを飲むメソッド
+  drink aCup ozDrank = if ozDiff >= 0
+                       then cup ozDiff
+                       else cup 0
+    where f10z = getOz aCup
+          ozDiff = f10z - ozDrank
+  
+  -- 空かどうかを判定するヘルパーメソッド
+  isEmpty aCup = getOz aCup == 0
+  -- 畳み込みでコーヒーをすする
+  -- afterManySips = foldl drink aCup [1, 1 , 1 ,1]
+  
+
   
   main = do
     print "ababa"
