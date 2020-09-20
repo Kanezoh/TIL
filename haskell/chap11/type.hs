0f5559e867a84f1ed185a69dfe8cbc233550335f
Kanezoh <asfaltmanta114@gmail.com>
2020-09-20 17:20:42 +0900

    型シグネチャの書き方

--- haskell/chap11/type.hs
+++ haskell/chap11/type.hs
@@ -1,9 +1,21 @@
 module Main where
   halve :: Int -> Int
   halve n = n `div` 2
+
+  -- Int→Stringの型変換
   printDouble :: Int -> String
   printDouble n = show (n * 2)
 
+  -- 複数の引数の型シグネチャ
+  makeAddress :: Int -> String -> String -> (Int, String, String)
+  makeAddress number street town = (number, street, town)
+
+  -- ファーストクラス関数の型
+  -- 関数の部分は丸括弧で引数と返り値を囲む
+  ifEven :: (Int -> Int) -> Int -> Int
+  ifEven f n = if even n
+               then f n
+               else n
   main = do
   -- show 数字→文字列
   print (show 6) -- => "6"
