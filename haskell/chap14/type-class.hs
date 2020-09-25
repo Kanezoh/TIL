88f2e843c051335288e173a53d9e92ef33066bb4
Kanezoh <asfaltmanta114@gmail.com>
2020-09-25 16:46:37 +0900

    課題

--- haskell/chap14/type-class.hs
+++ haskell/chap14/type-class.hs
@@ -1,4 +1,5 @@
-data SixSidedDie = S1 | S2 | S3 | S4 | S5 | S6 deriving (Eq, Ord)
+{-# LANGUAGE BlockArguments #-}
+data SixSidedDie = S1 | S2 | S3 | S4 | S5 | S6 deriving Enum
 
 instance Show SixSidedDie where
   show S1 = "one"
@@ -24,18 +25,41 @@ instance Show SixSidedDie where
 --  compare S6 _  = GT
 --  compare _ S6  = LT
 
-instance Enum SixSidedDie where
-  toEnum 0 = S1
-  toEnum 1 = S2
-  toEnum 2 = S3
-  toEnum 3 = S4
-  toEnum 4 = S5
-  toEnum 5 = S6
-  toEnum _ = error "No such value"
+-- Enumのインスタンスを自分で定義
+--instance Enum SixSidedDie where
+--  toEnum 0 = S1
+--  toEnum 1 = S2
+--  toEnum 2 = S3
+--  toEnum 3 = S4
+--  toEnum 4 = S5
+--  toEnum 5 = S6
+--  toEnum _ = error "No such value"
+--
+--  fromEnum S1 = 0
+--  fromEnum S2 = 1
+--  fromEnum S3 = 2
+--  fromEnum S4 = 3
+--  fromEnum S5 = 4
+--  fromEnum S6 = 5
 
-  fromEnum S1 = 0
-  fromEnum S2 = 1
-  fromEnum S3 = 2
-  fromEnum S4 = 3
-  fromEnum S5 = 4
-  fromEnum S6 = 5
+instance Eq SixSidedDie where
+  (==) s1 s2 = if show s1 == show s2
+               then True
+               else False
+
+instance Ord SixSidedDie where
+  compare s1 s2
+    | fromEnum s1 > fromEnum s2 = GT
+    | fromEnum s1 == fromEnum s2 = EQ
+    | otherwise = LT
+
+newtype Name = Name (String, String) deriving (Show, Eq)
+
+instance Ord Name where
+  compare (Name(f1,l1)) (Name (f2,l2)) = compare (l1,f1) (l2,f2)
+
+data FiveSidedDie = N1 | N2 | N3 | N4 | N5 deriving (Eq, Ord, Enum)
+class (Eq a, Enum a) => Die a where
+  roll :: Int -> a
+instance Die FiveSidedDie where
+  roll n = toEnum (n `mod` 5)
