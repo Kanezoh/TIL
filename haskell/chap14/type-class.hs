f859f59ca970c68640ecdd52d66329b74fb61401
Kanezoh <asfaltmanta114@gmail.com>
2020-09-25 15:42:43 +0900

    Enumインスタンスを実装

--- haskell/chap14/type-class.hs
+++ haskell/chap14/type-class.hs
@@ -1,4 +1,4 @@
-data SixSidedDie = S1 | S2 | S3 | S4 | S5 | S6
+data SixSidedDie = S1 | S2 | S3 | S4 | S5 | S6 deriving (Eq, Ord)
 
 instance Show SixSidedDie where
   show S1 = "one"
@@ -8,11 +8,34 @@ instance Show SixSidedDie where
   show S5 = "five"
   show S6 = "six"
 
-instance Eq SixSidedDie where
-  (==) S6 S6 = True
-  (==) S5 S5 = True
-  (==) S4 S4 = True
-  (==) S3 S3 = True
-  (==) S2 S2 = True
-  (==) S1 S1 = True
-  (==) _ _   = False
+-- Eq, Ordのインスタンスを自分で定義する場合
+-- EqはまだしもOrdは多すぎる、データの順番に気をつけてderivingから派生させるのが良い
+--instance Eq SixSidedDie where
+--  (==) S6 S6 = True
+--  (==) S5 S5 = True
+--  (==) S4 S4 = True
+--  (==) S3 S3 = True
+--  (==) S2 S2 = True
+--  (==) S1 S1 = True
+--  (==) _ _   = False
+--
+--instance Ord SixSidedDie where
+--  compare S6 S6 = EQ
+--  compare S6 _  = GT
+--  compare _ S6  = LT
+
+instance Enum SixSidedDie where
+  toEnum 0 = S1
+  toEnum 1 = S2
+  toEnum 2 = S3
+  toEnum 3 = S4
+  toEnum 4 = S5
+  toEnum 5 = S6
+  toEnum _ = error "No such value"
+
+  fromEnum S1 = 0
+  fromEnum S2 = 1
+  fromEnum S3 = 2
+  fromEnum S4 = 3
+  fromEnum S5 = 4
+  fromEnum S6 = 5
