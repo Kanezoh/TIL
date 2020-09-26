fce3a994ea6202e7293284729e3100a0b0e7b634
Kanezoh <asfaltmanta114@gmail.com>
2020-09-26 23:46:42 +0900

    15章まとめ

--- haskell/chap15/xor.hs
+++ haskell/chap15/xor.hs
@@ -59,3 +59,19 @@ applyOTP' pad plainText = map (\pair -> (fst pair) `xor` (snd pair)) (zip padBit
 applyOTP :: String -> String -> String
 applyOTP pad plainText = map bitsToChar bitList
   where bitList = applyOTP' pad plainText
+
+class Cipher a where
+  encode :: a -> String -> String
+  decode :: a -> String -> String
+
+data Rot = Rot
+
+instance Cipher Rot where
+  encode Rot text = rotEncoder text
+  decode Rot text = rotDecoder text
+
+data OneTimePad = OTP String
+
+instance Cipher OneTimePad where
+  encode (OTP pad) text = applyOTP pad text
+  decode (OTP pad) text = applyOTP pad text
