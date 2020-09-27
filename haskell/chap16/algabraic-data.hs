26683f53429bcbc64dfd9e02afa784725fe35cf7
Kanezoh <asfaltmanta114@gmail.com>
2020-09-27 11:37:58 +0900

    直和型を学習

--- haskell/chap16/algabraic-data.hs
+++ haskell/chap16/algabraic-data.hs
@@ -14,3 +14,25 @@ data Book = Book AuthorName String String Int Double
 --   , year :: Int
 --   , price :: Double
 -- }
+
+-- 直和型 複数の型をORで組み合わせる
+
+-- 一般的な直和型のBool
+data Bool = False | True
+
+-- 直和型を使ってミドルネームの有無、2つのコンストラクタを定義できる例
+type FirstName = String
+type LastName = String
+type MiddleName = String
+
+data Name = Name FirstName LastName
+            | NameWithMiddle FirstName MiddleName LastName
+            | TwoInitialsWithLast Char Char LastName
+
+-- Author または ArtistとなるCreator型
+newtype Author = Author Name
+data Artist = Person Name | Band String
+data Creator = AuthorCreator Author | ArtistCreator Artist
+
+hploveCraft :: Creator
+hploveCraft = AuthorCreator (Author (TwoInitialsWithLast 'H' 'P' "Lovecraft"))
