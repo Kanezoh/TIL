2fa4d5e26aeba1b781569ce551cdac0d4a27d81e
Kanezoh <asfaltmanta114@gmail.com>
2020-09-27 12:18:23 +0900

    madeby関数を定義

--- haskell/chap16/algabraic-data.hs
+++ haskell/chap16/algabraic-data.hs
@@ -1,6 +1,6 @@
 -- 直積型 複数の型を論理積で組み合わせる型
 data AuthorName = AuthorName String String
-data Book = Book AuthorName String String Int Double
+-- data Book = Book AuthorName String String Int Double
 
 -- レコード構文
 -- data AuthorName = AuthorName {
@@ -36,3 +36,49 @@ data Creator = AuthorCreator Author | ArtistCreator Artist
 
 hploveCraft :: Creator
 hploveCraft = AuthorCreator (Author (TwoInitialsWithLast 'H' 'P' "Lovecraft"))
+
+-- ブックストアプログラムを作成
+-- 本
+data Book = Book {
+    author    :: Creator
+  , isbn      :: String
+  , bookTitle :: String
+  , bookYear  :: Int
+  , bookPrice :: Double
+}
+-- レコード
+data VinylRecord = VinylRecord {
+    artist      :: Creator
+  , recordTitle :: String
+  , recordYear  :: String
+  , recordPrice :: Double
+}
+-- おもちゃ
+data CollectibleToy = CollectibleToy {
+    name        :: String
+  , description :: String
+  , toyPrice    :: Double
+}
+-- パンフレット
+data Pamphlet = Pamphlet {
+    title       :: String
+  , description :: String
+  , contact     :: String
+}
+
+-- 商品を表すデータ型
+data StoreItem = BookItem Book
+                 | RecordItem VinylRecord
+                 | ToyItem CollectibleToy
+                 | PamphletItem Pamphlet
+
+-- 全てのItemのpriceを取得する関数
+price :: StoreItem -> Double
+price (BookItem book) = bookPrice book
+price (RecordItem record) = recordPrice record
+price (ToyItem toy) = toyPrice toy
+price (PamphletItem Pamphlet) = 0
+
+madeby :: StoreItem -> String
+madeby (BookItem book) = show (author book)
+madeby (recordItem record) = show (artist record)
