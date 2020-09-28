2144229f8528661aa2df54a584442072bed45761
Kanezoh <asfaltmanta114@gmail.com>
2020-09-29 06:49:49 +0900

    セミグループ

--- haskell/chap17/compose.hs
+++ haskell/chap17/compose.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Semigroup
 
 -- composeと呼ばれる高階関数。 .で関数を合成できる
 myLast :: [a] -> a
@@ -17,3 +18,25 @@ myAll testFunc = foldr (&&) True . map testFunc
 -- リストの少なくとも一つの要素で、ある特性がTrueかどうか
 myAny :: (a -> Bool) -> [a] -> Bool
 myAny testFunc = foldr (||) True . map testFunc
+
+-- セミグループ、同じ型のインスタンスを組み合わせる
+instance Semigroup Integer where
+  (<>) x y = x + y
+
+-- 色を組み合わせるセミグループ
+data Color = Red | Yellow | Blue | Green | Purple | Orange | Brown deriving (Show, Eq)
+
+instance Semigroup Color where
+  (<>) Red Blue = Purple
+  (<>) Blue Red = Purple
+  (<>) Yellow Blue = Green
+  (<>) Blue Yellow = Green
+  (<>) Yellow Red = Orange
+  (<>) Red Yellow = Green
+  (<>) a b | a == b = a
+           | all (`elem` [Red, Blue, Purple]) [a,b] = Purple
+           | all (`elem` [Blue, Yellow, Green]) [a,b] = Green
+           | all (`elem` [Red, Yellow, Orange]) [a,b] = Orange
+           | otherwise = Brown
+
+
