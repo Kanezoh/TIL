06a9ad28252f63cabb82694672fa039a4cc29d95
Kanezoh <asfaltmanta114@gmail.com>
2020-10-04 12:50:27 +0900

    フィボナッチ数列

--- haskell/chap21/io.hs
+++ haskell/chap21/io.hs
@@ -1,3 +1,5 @@
+import qualified Data.Map as Map
+
 helloPerson :: String -> String
 helloPerson name = "Hello" ++ " " ++ name ++ "!"
 
@@ -22,3 +24,27 @@ main = do
 -- 引数なしでIOを返す
 -- 全ての関数が引数を受け取らなければならないという原則に反している
 -- getLine :: IO String
+
+-- maybe doで書き直す
+
+names :: Map.Map Int String
+names = Map.fromList [(1, "Bob")]
+
+maybeMain :: Maybe String
+maybeMain = do
+  name <- Map.lookup 1 names
+  let statement = helloPerson name
+  return statement
+
+-- n番目のフィボナッチ数列を出力
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib(n - 2)
+
+fibMain :: IO()
+fibMain = do
+  putStrLn "Enter the number"
+  index <- getLine
+  let ans = fib (read index)
+  putStrLn (show ans)
