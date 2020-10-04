47cd061755fb0cff09e9c8690f68702ced05fc07
Kanezoh <asfaltmanta114@gmail.com>
2020-10-04 19:51:59 +0900

    ストリームで入力を受け取る

--- haskell/chap22/sum_lazy.hs
+++ haskell/chap22/sum_lazy.hs
@@ -1,7 +1,15 @@
+import Data.List
+import Data.List.Split
+
+myLines = splitOn "\n"
+toInts :: String -> [Int]
+toInts = map read . lines
+
 main :: IO()
 main = do
   userInput <- getContents
-  mapM_ print userInput
+  let numbers = toInts userInput
+  print (sum numbers)
 
 -- クイックチェック3 反転して出力
 -- reverseMain :: IO()
@@ -9,3 +17,11 @@ main = do
 -- userInput <- getContents
 --  let reversed = reverse userInput
 --  print reversed
+
+-- クイックチェック4 入力の2乗の総和
+squareSum :: IO ()
+squareSum = do
+  userInput <- getContents
+  let numbers = toInts userInput
+  let squareNumbers = map (\x -> x^2) numbers
+  print (sum squareNumbers)
