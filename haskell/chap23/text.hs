4dd2bb72c3b54bcd859e2aad4d434a098333cbb4
Kanezoh <asfaltmanta114@gmail.com>
2020-10-05 11:02:02 +0900

    テキストユーティリティ

--- haskell/chap23/text.hs
+++ haskell/chap23/text.hs
@@ -21,3 +21,41 @@ myWord = "dog"
 -- 有効にする方法
 -- 1. コンパイル時に -XOverloadedStrings オプションを付ける
 -- 2. {-# LANGUAGE OverloadedStrings #-} をファイルの先頭に付ける
+
+-- テキストユーティリティ
+
+-- lines 改行文字を取り除く
+sampleInput :: T.Text
+sampleInput = "this\nis\ninput"
+
+-- T.lines sampleInput
+-- => ["this","is","input"]
+
+-- words 改行文字、ホワイトスペース文字を取り除く
+someText :: T.Text
+someText = "Some\ntext for\tyou"
+
+-- T.words someText
+-- => ["Some","text","for","you"]
+
+-- splitOn 
+breakText :: T.Text
+breakText = "simple"
+
+exampleText :: T.Text
+exampleText = "This is simple to do"
+
+-- T.splitOn breakText exampleText
+-- ["This is "," to do"]
+
+-- unlines, unwords Textをホワイトベースで結合する
+
+-- T.unlines (T.lines sampleInput)
+-- "this\nis\ninput\n"
+
+-- T.unwords(T.words someText)
+-- "Some text for you"
+
+-- intercalate splitOnと逆
+-- T.intercalate breakText (T.splitOn breakText exampleText)
+-- => "This is simple to do"
