507fe3c2627fe05d64ea714fc1e2e4a9121eb673
Kanezoh <asfaltmanta114@gmail.com>
2020-10-05 21:10:14 +0900

    text stream

--- haskell/chap23/question.hs
+++ haskell/chap23/question.hs
@@ -1,13 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+--import qualified Data.Text as T
+--import qualified Data.Text.IO as TIO
+import Data.List
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as TIO
 
-helloPerson :: T.Text -> T.Text
-helloPerson name = T.pack("Hello" ++ " " ++ T.unpack(name) ++ "!")
+-- 練習問題1
+-- helloPerson :: T.Text -> T.Text
+-- helloPerson name = mconcat ["Hello", name, "!"]
+-- 
+-- main :: IO()
+-- main = do
+--   putStrLn "Hello, What's your name?"
+--   name <- getLine
+--   let statement = helloPerson (T.pack(name))
+--   TIO.putStrLn statement
+
+toInts :: T.Text -> [Int]
+toInts = map (read . T.unpack) . T.lines
 
 main :: IO()
 main = do
-  putStrLn "Hello, What's your name?"
-  name <- getLine
-  let statement = helloPerson (T.pack(name))
-  TIO.putStrLn statement
+  userInput <- TIO.getContents
+  let numbers = toInts userInput
+  TIO.putStrLn ((T.pack . show . sum) numbers)
