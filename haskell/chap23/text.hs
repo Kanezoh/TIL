ace184202e2fe2fc9014ec9625db951992182a75
Kanezoh <asfaltmanta114@gmail.com>
2020-10-05 10:31:51 +0900

    言語拡張

--- haskell/chap23/text.hs
+++ haskell/chap23/text.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 import qualified Data.Text as T
 
 -- String→リスト Text→配列
 -- 遅延評価をしないのが特徴
 
+-- 相互変換
 firstWord :: String
 firstWord = "pessimism"
 
@@ -11,3 +13,11 @@ secondWord = T.pack firstWord
 
 thirdWord :: String
 thirdWord = T.unpack secondWord
+
+-- 以下のコードは何もしていないとエラーになる
+myWord :: T.Text
+myWord = "dog"
+
+-- 有効にする方法
+-- 1. コンパイル時に -XOverloadedStrings オプションを付ける
+-- 2. {-# LANGUAGE OverloadedStrings #-} をファイルの先頭に付ける
