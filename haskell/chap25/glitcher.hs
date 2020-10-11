960ba70942be6d1a3188a01c64b9be8fff683159
Kanezoh <asfaltmanta114@gmail.com>
2020-10-11 22:18:25 +0900

    glitch

--- haskell/chap25/glitcher.hs
+++ haskell/chap25/glitcher.hs
@@ -1,14 +1,37 @@
 import System.Environment
+import System.Random
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
+
+intToChar :: Int -> Char
+intToChar int = toEnum safeInt
+  where safeInt = int `mod` 255
+
+intToBC :: Int -> BC.ByteString
+intToBC int = BC.pack [intToChar int]
+
+replaceByte :: Int -> Int -> BC.ByteString -> BC.ByteString
+-- 新しいByteStringは前の部分と後ろの部分をnewCharで繋いだもの
+replaceByte loc charVal bytes = mconcat [before, newChar, after]
+  -- パターンマッチングを使って2つの値を2つの変数に一度に割り当てる
+  where (before, rest) = BC.splitAt loc bytes
+        after = BC.drop 1 rest
+        newChar = intToBC charVal
+
+randomReplaceByte :: BC.ByteString -> IO BC.ByteString
+randomReplaceByte bytes = do
+  let bytesLength = BC.length bytes
+  location <- randomRIO (1, bytesLength)
+  charVal  <- randomRIO (0, 255)
+  return (replaceByte location charVal bytes)
+
 
 main :: IO()
 main = do
   args <- getArgs
   let fileName = head args
   imageFile <- BC.readFile fileName
-  -- TODO returnはあとでバイナリデータを変更する関数に置き換える
-  glitched <- return imageFile
+  glitched <- randomReplaceByte imageFile
   let glitchedFileName = mconcat ["glitched_", fileName]
   BC.writeFile glitchedFileName glitched
   print "all done"
