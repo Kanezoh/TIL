4cae72a1a03c07b9a8c3f01ed7ed38065dc6be09
Kanezoh <asfaltmanta114@gmail.com>
2020-10-11 22:52:52 +0900

    ランダムにglitch

--- haskell/chap25/glitcher.hs
+++ haskell/chap25/glitcher.hs
@@ -18,6 +18,7 @@ replaceByte loc charVal bytes = mconcat [before, newChar, after]
         after = BC.drop 1 rest
         newChar = intToBC charVal
 
+-- ランダムで指定したバイト文字を入れ替える
 randomReplaceByte :: BC.ByteString -> IO BC.ByteString
 randomReplaceByte bytes = do
   let bytesLength = BC.length bytes
@@ -25,13 +26,35 @@ randomReplaceByte bytes = do
   charVal  <- randomRIO (0, 255)
   return (replaceByte location charVal bytes)
 
+-- ファイルの一部を並べ替える
+sortSection :: Int -> Int -> BC.ByteString -> BC.ByteString
+sortSection start size bytes = mconcat [before, changed, after]
+  where (before, rest)  = BC.splitAt start bytes
+        (target, after) = BC.splitAt size rest
+        changed         = BC.reverse (BC.sort target)
+
+-- sortSection関数をランダム化する
+randomSortSection :: BC.ByteString -> IO BC.ByteString
+randomSortSection bytes = do
+  -- 並べ替えるセクションのサイズを適当に選択
+  let sectionSize = 25
+  let bytesLength = BC.length bytes
+  -- randomRIOを使って並べ替えを開始する位置を決定
+  start <- randomRIO (0, bytesLength - sectionSize)
+  return (sortSection start sectionSize bytes)
 
 main :: IO()
 main = do
   args <- getArgs
   let fileName = head args
   imageFile <- BC.readFile fileName
-  glitched <- randomReplaceByte imageFile
+  glitched <- randomSortSection imageFile
   let glitchedFileName = mconcat ["glitched_", fileName]
   BC.writeFile glitchedFileName glitched
   print "all done"
+
+-- クイックチェック3
+randomChar :: IO Char
+randomChar = do
+  randomInt <- randomRIO(0, 255)
+  return (toEnum randomInt)
