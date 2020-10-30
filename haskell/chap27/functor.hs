4da6308fba7d86df71cf0b20b637a73dad416e94
Kanezoh <asfaltmanta114@gmail.com>
2020-10-30 23:13:41 +0900

    functorを使用する

--- haskell/chap27/functor.hs
+++ haskell/chap27/functor.hs
@@ -9,5 +9,17 @@ incMaybe (Just n) = Just (n + 1)
 incMaybe Nothing = Nothing
 
 reverseMaybe :: Maybe String -> Maybe String
-reverseMaybe (Just str) = Just (reverse str)
-reverseMaybe Nothing = Nothing
+-- Functorを使わない場合
+--reverseMaybe :: Maybe String -> Maybe String
+--reverseMaybe (Just str) = Just (reverse str)
+--reverseMaybe Nothing = Nothing
+reverseMaybe str = reverse <$> str
+
+-- Functor で型クラスのコンテキストで関数を適用する
+-- <$>はfmapのエイリアス
+successStr :: Maybe String
+successStr = show <$> successfulRequest
+
+failStr :: Maybe String
+failStr = show <$> failedRequest
+
