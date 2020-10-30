957a03f3c941dc670131d4186bce9fadda1cd65e
Kanezoh <asfaltmanta114@gmail.com>
2020-10-30 21:19:30 +0900

    chap27

---
+++ haskell/chap27/functor.hs
@@ -0,0 +1,13 @@
+successfulRequest :: Maybe Int
+successfulRequest = Just 6
+
+failedRequest :: Maybe Int
+failedRequest = Nothing
+
+incMaybe :: Maybe Int -> Maybe Int
+incMaybe (Just n) = Just (n + 1)
+incMaybe Nothing = Nothing
+
+reverseMaybe :: Maybe String -> Maybe String
+reverseMaybe (Just str) = Just (reverse str)
+reverseMaybe Nothing = Nothing
