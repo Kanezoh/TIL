ff20e3c44df1d8041e90cd5f16fb75846c10feec
Kanezoh <asfaltmanta114@gmail.com>
2020-11-05 23:38:32 +0900

    テストユーザーの作成

---
+++ haskell/chap29/user.hs
@@ -0,0 +1,22 @@
+data User = User { name    :: String
+                 , gamerId :: Int
+                 , score   :: Int } deriving Show
+
+testNames :: [String]
+testNames = [ "John Smith"
+            , "Robert'); DROP TABLE Students; --"
+            , "Christina NULL"
+            , "Randall Nunroe"]
+
+testIds :: [Int]
+testIds = [ 1337
+          , 0123
+          , 999999 ]
+
+testScores :: [Int]
+testScores = [ 0
+             , 100000
+             , -999999]
+
+testData :: [User]
+testData = pure User <*> testNames <*> testIds <*> testScores
