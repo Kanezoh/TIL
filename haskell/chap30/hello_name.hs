84db38a3ab58107360136696a9e46a8fb16207af
Kanezoh <asfaltmanta114@gmail.com>
2020-12-08 23:38:59 +0900

    monad prac-1

--- haskell/chap30/hello_name.hs
+++ haskell/chap30/hello_name.hs
@@ -6,3 +6,7 @@ nameStatement name = "Hello, " ++ name ++ "!"
 
 helloName :: IO()
 helloName = askForName >> getLine >>= (\name -> return (nameStatement name)) >>= putStrLn
+
+-- 練習問題1
+allFmapM :: Monad m => (a -> b) -> m a -> m b
+allFmapM func val = val >>= (\x -> return (func x))
