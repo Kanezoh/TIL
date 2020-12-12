3e92fe423f091a21c038e1b8f1ef8f0291e6e744
Kanezoh <asfaltmanta114@gmail.com>
2020-12-13 00:16:39 +0900

    prac2

--- haskell/chap31/hello_name.hs
+++ haskell/chap31/hello_name.hs
@@ -117,3 +117,15 @@ assessCandidate candidates = do
                   then "passed"
                   else "failed"
   return statement
+
+-- 練習問題2
+listMain :: [String]
+listMain = do
+  size1 <- [1,2,3]
+  cost1 <- [100,200,300]
+  size2 <- [4,5,6]
+  cost2 <- [400,500,600]
+  let pizza1 = (size1,cost1)
+  let pizza2 = (size2,cost2)
+  let betterPizza = comparePizzas pizza1 pizza2
+  return (describePizza betterPizza)
