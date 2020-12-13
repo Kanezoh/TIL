8af86722734462bc25f3332568c076f020404483
Kanezoh <asfaltmanta114@gmail.com>
2020-12-13 19:11:24 +0900

    リスト内包であらゆる組み合わせをえる

--- haskell/chap32/list_monad.hs
+++ haskell/chap32/list_monad.hs
@@ -9,3 +9,16 @@ powersOfTwo n = do
 -- mapで
 -- powersOfTwo :: Int -> [Int]
 -- powersOfTwo n = map (\x -> 2^x) [1..n]
+
+powersOfTwoAndThree :: Int -> [(Int, Int)]
+powersOfTwoAndThree n = do
+  value <- [1 .. n]
+  let powersOfTwo = 2^value
+  let powersOfThree = 3^value
+  return (powersOfTwo, powersOfThree)
+
+allEvenOdds :: Int -> [(Int,Int)]
+allEvenOdds n = do
+  evenValue <- [2,4 .. n]
+  oddValue <- [1,3 .. n]
+  return (evenValue,oddValue)
