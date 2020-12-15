6cc6097de23a4da080b59206aced648330867eaa
Kanezoh <asfaltmanta114@gmail.com>
2020-12-15 17:06:19 +0900

    リファクタリング

--- haskell/chap34/Main.hs
+++ haskell/chap34/Main.hs
@@ -1,12 +1,12 @@
 module Main where
-import qualified Palindrome
+import Palindrome
 
 -- main IOアクションでユーザー入力を読み取り、回文判定
 main :: IO()
 main = do
   print "Enter a word and I'll let you know if it's a palindrome!"
   text <- getLine
-  let response = if Palindrome.isPalindrome text
+  let response = if isPalindrome text
                  then "it is!"
                  else "it's not!"
   print response
