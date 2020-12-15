9bb0648a951631ce3dae3e9367ff2bc51bd684f5
Kanezoh <asfaltmanta114@gmail.com>
2020-12-15 17:05:59 +0900

    Palindromeモジュールをインポート

--- haskell/chap34/Main.hs
+++ haskell/chap34/Main.hs
@@ -1,15 +1,12 @@
 module Main where
-
--- isPalindrome
-isPalindrome :: String -> Bool
-isPalindrome text = text == reverse text
+import qualified Palindrome
 
 -- main IOアクションでユーザー入力を読み取り、回文判定
 main :: IO()
 main = do
   print "Enter a word and I'll let you know if it's a palindrome!"
   text <- getLine
-  let response = if isPalindrome text
+  let response = if Palindrome.isPalindrome text
                  then "it is!"
                  else "it's not!"
   print response
