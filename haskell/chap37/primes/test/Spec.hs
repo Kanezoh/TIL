5929860676eddf12332033fb04dcc1ab243b2a71
Kanezoh <asfaltmanta114@gmail.com>
2020-12-20 00:00:32 +0900

    値が妥当かどうかを検証

--- haskell/chap37/primes/test/Spec.hs
+++ haskell/chap37/primes/test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.QuickCheck
+import Primes
+import Data.Maybe
+
+prop_validPrimesOnly val = if val < 0 || val >= last primes
+                           then result == Nothing
+                           else isJust result
+  where result = isPrime val
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheck prop_validPrimesOnly
