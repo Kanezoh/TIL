5a7fc0aa3b37bf26c94bd6f428a7102383cdb76c
Kanezoh <asfaltmanta114@gmail.com>
2020-12-19 23:41:07 +0900

    素数リスト生成アルゴリズムを実装

--- haskell/chap37/primes/src/Primes.hs
+++ haskell/chap37/primes/src/Primes.hs
@@ -2,3 +2,12 @@ module Primes where
 
 primes :: [Int]
 primes = [1.. ]
+
+-- 「エラトステネスのふるい」というアルゴリズム
+-- [2,3,4,5]のような並びであれば、まずは2を最初の素数として
+-- 他の要素を2で割る、割り切れたら素数ではないので除外
+-- これを繰り返す
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (nextPrime:rest) = nextPrime : sieve noFactors
+  where noFactors = filter (not . (== 0) . (`mod` nextPrime)) rest
