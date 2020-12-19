9e83a490dc4a27f3c7c5ef58c35d90aa9b4f3ae6
Kanezoh <asfaltmanta114@gmail.com>
2020-12-20 00:29:18 +0900

    isPrimeのプロパティテスト

--- haskell/chap37/primes/test/Spec.hs
+++ haskell/chap37/primes/test/Spec.hs
@@ -2,10 +2,28 @@ import Test.QuickCheck
 import Primes
 import Data.Maybe
 
+-- 0未満、最大値より上の入力の時はNothingを返すかどうかをテスト
 prop_validPrimesOnly val = if val < 0 || val >= last primes
                            then result == Nothing
                            else isJust result
   where result = isPrime val
+
+-- 素数と判定されたものが本当に素数かをテスト
+prop_primesArePrime val = if result == Just True
+                          then length divisors == 0
+                          else True
+  where result = isPrime val
+        divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
+
+prop_nonpPrimesAreComposite val = if result == Just False
+                                  then length divisors > 0
+                                  else True
+  where result = isPrime val
+        divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
+
 main :: IO ()
 main = do
   quickCheck prop_validPrimesOnly
+  quickCheckWith stdArgs { maxSuccess = 1000 } prop_primesArePrime
+  quickCheckWith stdArgs { maxSuccess = 1000 } prop_nonpPrimesAreComposite
+
