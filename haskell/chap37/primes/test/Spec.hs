02d0fe3407e77b43c25967309d9b229bd3563b48
Kanezoh <asfaltmanta114@gmail.com>
2020-12-20 09:26:18 +0900

    プロパティテストを追加

--- haskell/chap37/primes/test/Spec.hs
+++ haskell/chap37/primes/test/Spec.hs
@@ -21,9 +21,24 @@ prop_nonpPrimesAreComposite val = if result == Just False
   where result = isPrime val
         divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
 
+-- 素因数の積が元の値になる
+prop_factorsMakeOriginal val = if result == Nothing
+                               then True
+                               else product (fromJust result) == val
+  where result = primeFactors val
+
+-- 全ての素因数が素数である
+prop_allFactorsPrime val = if result == Nothing
+                           then True
+                           else all (== Just True) resultsPrime
+  where result = primeFactors val
+        resultsPrime = map isPrime (fromJust result)
+
 main :: IO ()
 main = do
   quickCheck prop_validPrimesOnly
   quickCheckWith stdArgs { maxSuccess = 1000 } prop_primesArePrime
   quickCheckWith stdArgs { maxSuccess = 1000 } prop_nonpPrimesAreComposite
+  quickCheck prop_factorsMakeOriginal
+  quickCheck prop_allFactorsPrime
 
