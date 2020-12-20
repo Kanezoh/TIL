8fccb4a9ad3117ee0424e86f3f0028de4085f972
Kanezoh <asfaltmanta114@gmail.com>
2020-12-20 09:19:34 +0900

    素因数分解を実装

--- haskell/chap37/primes/src/Primes.hs
+++ haskell/chap37/primes/src/Primes.hs
@@ -16,3 +16,19 @@ isPrime :: Int -> Maybe Bool
 isPrime n | n < 2 = Nothing
           | n >= last primes = Nothing
           | otherwise = Just (n `elem` primes)
+
+-- 素因数分解
+unsafePrimeFactors :: Int -> [Int] -> [Int]
+unsafePrimeFactors 0 [] = []
+unsafePrimeFactors n [] = []
+unsafePrimeFactors n (next:primes) = 
+  if n `mod` next == 0
+  then next:unsafePrimeFactors (n `div` next) (next:primes)
+  else unsafePrimeFactors n primes
+
+-- Maybeで安全にした素因数分解
+primeFactors :: Int -> Maybe [Int]
+primeFactors n | n < 2 = Nothing
+               | n >= last primes = Nothing
+               | otherwise = Just (unsafePrimeFactors n primesLessThanN)
+  where primesLessThanN = filter (<= n) primes
