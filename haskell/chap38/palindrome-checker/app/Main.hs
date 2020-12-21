9d7a4a1ba68012bf467450e0bee5f9e9444900a4
Kanezoh <asfaltmanta114@gmail.com>
2020-12-21 22:39:32 +0900

    wallオプション

--- haskell/chap38/palindrome-checker/app/Main.hs
+++ haskell/chap38/palindrome-checker/app/Main.hs
@@ -2,5 +2,16 @@ module Main where
 
 import Lib
 
+myTake :: Int -> [a] -> [a]
+myTake 0 _ = []
+myTake n xs = head xs : myTake (n-1) (tail xs)
+
+-- コンパイル時に警告が出る書き方(-Wallオプションを付けている場合)
+-- 空のリストに対するパターンマッチングが必要
+-- myTakePM 0 _ [] = []
+myTakePM :: Int -> [a] -> [a]
+myTakePM 0 _ = []
+myTakePM n (x:xs) = x : myTakePM (n-1) xs
+
 main :: IO ()
 main = return ()
