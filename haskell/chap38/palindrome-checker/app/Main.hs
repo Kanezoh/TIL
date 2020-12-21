22578f71437f6ac2ec5bac11631fffbb49eceafe
Kanezoh <asfaltmanta114@gmail.com>
2020-12-21 22:43:03 +0900

    エラー処理

--- haskell/chap38/palindrome-checker/app/Main.hs
+++ haskell/chap38/palindrome-checker/app/Main.hs
@@ -13,5 +13,11 @@ myTakePM :: Int -> [a] -> [a]
 myTakePM 0 _ = []
 myTakePM n (x:xs) = x : myTakePM (n-1) xs
 
+-- エラーのスロー
+-- コンパイル時にエラーが出ないのであまり推奨されない
+myHead :: [a] -> a
+myHead [] = error "empty list!"
+myHead (x:_) = x
+
 main :: IO ()
 main = return ()
