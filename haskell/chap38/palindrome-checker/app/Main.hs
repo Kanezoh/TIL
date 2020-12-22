5610eb3b8d2339421e77a71276485232984152de
Kanezoh <asfaltmanta114@gmail.com>
2020-12-22 10:38:42 +0900

    Eitherで複数の型を指定

--- haskell/chap38/palindrome-checker/app/Main.hs
+++ haskell/chap38/palindrome-checker/app/Main.hs
@@ -26,5 +26,44 @@ maybeHead(x:_) = Just x
 -- (+2) <$> maybeHead [1]
 -- (:) <$> maybeHead [1,2,3] <*> Just []
 
+myTakeSafer :: Int -> Maybe [a] -> Maybe [a]
+myTakeSafer 0 _ = Just []
+myTakeSafer n (Just xs) = (:) <$> maybeHead xs
+                              <*> myTakeSafer (n-1) (Just (tail xs))
+
+-- Maybeを使うのはいいが、Nothingが何を返すのか曖昧
+-- そもそも意図してない入力なのか、入力は意図しているが単に失敗したのか
+-- そのためにEither型を使う
+-- data Either a b = Left a | Right b
+-- Leftが失敗、Rightが成功
+eitherHead :: [a] -> Either String a
+eitherHead [] = Left "There is no head because the list is empty"
+eitherHead (x:_) = Right x
+
+intExample :: [Int]
+intExample = [1,2,3]
+-- eitherHead intExample => Right 1
+
+intExampleEmpty :: [Int]
+intExampleEmpty = []
+-- eitherHead intExampleEmpty => Left "There is no head because the list is empty"
+
+charExample :: [Char]
+charExample = "cat"
+-- eitherHead charExample => Right 'c'
+
+charExampleEmpty :: [Char]
+charExampleEmpty = []
+-- eitherHead charExampleEmpty => Left "There is no head because the list is empty"
+
+-- 複数のエラー型を指定することができる
+maxN = 10
+primes = [2,3,5,7]
+isPrime :: Int -> Either String Bool
+isPrime n
+  | n < 2 = Left "Numbers less than 2 are not candidates for primes"
+  | n > maxN = Left "Value exceeds limits of prime checker"
+  | otherwise = Right (n `elem` primes)
+
 main :: IO ()
 main = return ()
