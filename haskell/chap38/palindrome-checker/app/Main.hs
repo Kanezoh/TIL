243ff445b814f9e9f08eebf9c90834257b98d27d
Kanezoh <asfaltmanta114@gmail.com>
2020-12-22 10:41:53 +0900

    独自のエラー型を定義

--- haskell/chap38/palindrome-checker/app/Main.hs
+++ haskell/chap38/palindrome-checker/app/Main.hs
@@ -56,13 +56,18 @@ charExampleEmpty :: [Char]
 charExampleEmpty = []
 -- eitherHead charExampleEmpty => Left "There is no head because the list is empty"
 
+data PrimeError = TooLarge | InvalidValue
+instance Show PrimeError where
+  show TooLarge = "Value exceed max bound"
+  show InvalidValue = "Value is not a valid candidate for prime checking"
+
 -- 複数のエラー型を指定することができる
 maxN = 10
 primes = [2,3,5,7]
-isPrime :: Int -> Either String Bool
+isPrime :: Int -> Either PrimeError Bool
 isPrime n
-  | n < 2 = Left "Numbers less than 2 are not candidates for primes"
-  | n > maxN = Left "Value exceeds limits of prime checker"
+  | n < 2 = Left InvalidValue
+  | n > maxN = Left TooLarge
   | otherwise = Right (n `elem` primes)
 
 main :: IO ()
