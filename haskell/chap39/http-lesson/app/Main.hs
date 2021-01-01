2baeeb9e4581bc24c38c34241abd729dce84d0e9
Kanezoh <asfaltmanta114@gmail.com>
2021-01-01 19:24:20 +0900

    必要なライブラリのインポート

--- haskell/chap39/http-lesson/app/Main.hs
+++ haskell/chap39/http-lesson/app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
-import Lib
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as LC
+import Network.HTTP.Simple
+
+myToken :: BC.ByteString
+myToken = ""
+noaaHost :: BC.ByteString
+noaaHost = "www.ncdc.noaa.gov"
+apiPath :: BC.ByteString
+apiPath = "/cdo-Web/api/v2/datasets"
 
 main :: IO ()
-main = someFunc
+main = print "hi"
