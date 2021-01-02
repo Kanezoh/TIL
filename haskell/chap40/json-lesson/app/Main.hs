7b7edf21c555d2aa8bd794f01f1d9c0fb588349f
Kanezoh <asfaltmanta114@gmail.com>
2021-01-02 09:50:04 +0900

    データ型とJSONの相互変換

--- haskell/chap40/json-lesson/app/Main.hs
+++ haskell/chap40/json-lesson/app/Main.hs
@@ -5,5 +5,27 @@ import Data.ByteString.Lazy as B
 import Data.ByteString.Lazy.Char8 as BC
 import GHC.Generics
 
+instance FromJSON Book
+instance ToJSON Book
+
+data Book = Book { title :: T.Text
+                 , author :: T.Text
+                 , year :: Int } deriving (Show, Generic)
+
+myBook :: Book
+myBook = Book { author="Will Kurt", title="Learn Haskell", year=2017}
+myBookJSON :: BC.ByteString
+myBookJSON = encode myBook
+rawJSON :: BC.ByteString
+rawJSON = "{\"author\":\"Emil Ciroan\",\"title\":\"A Short History of Decay\",\"year\":1949}"
+bookFromJSON :: Maybe Book
+bookFromJSON = decode rawJSON
+
+-- 間違ったJSON、Nothingを返す
+wrongJSON :: BC.ByteString
+wrongJSON = "{\"writer\":\"Emil Ciroan\",\"title\":\"A Short History of Decay\",\"year\":1949}"
+bookFromWrongJSON :: Maybe Book
+bookFromWrongJSON = decode wrongJSON
+
 main :: IO ()
 main = print "hi"
