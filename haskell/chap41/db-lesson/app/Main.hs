4aaf7c0627925cc41a63475120a9d88e6761b192
Kanezoh <asfaltmanta114@gmail.com>
2021-01-03 11:51:48 +0900

    Showのインスタンス化

--- haskell/chap41/db-lesson/app/Main.hs
+++ haskell/chap41/db-lesson/app/Main.hs
@@ -5,5 +5,26 @@ import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
 import Data.Time
 
+data Tool = Tool { toolId :: Int
+                 , name :: String
+                 , description :: String
+                 , lastReturned :: Day
+                 , timesBorrowed :: Int }
+data User = User { userId :: Int
+                 , userName :: String }                 
+
+instance Show User where
+  show user = mconcat [ show $ userId user, ".) ", userName user]
+
+instance Show Tool where
+  show tool = mconcat [ show $ toolId tool, ".) "
+                      , name tool
+                      , "\n description: "
+                      , description tool
+                      , "\n last returned: "
+                      , show $ lastReturned tool
+                      , "\n times borrowed: "
+                      , show $ timesBorrowed tool
+                      , "\n"]
 main :: IO ()
 main = print "db-lesson"
