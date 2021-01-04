84e8062f083ccc1d70c56d3f4d22cb5ef15fccec
Kanezoh <asfaltmanta114@gmail.com>
2021-01-04 10:31:08 +0900

    uarrayの基本操作

--- haskell/chap42/uarray.hs
+++ haskell/chap42/uarray.hs
@@ -13,3 +13,14 @@ oneIndexArray = array (1,10) $ zip [1 .. 10] $ cycle [True]
 
 qcArray :: UArray Int Bool
 qcArray = array (0,4) [(1,True), (2,True)]
+
+-- Intなので値は0で初期化
+beansInBuckets :: UArray Int Int
+beansInBuckets = array (0,3) []
+
+--  //演算子で値を更新する
+updateBiB :: UArray Int Int
+updateBiB = beansInBuckets // [(1,5), (3,6)]
+
+-- accum関数、二項関数,UArray,UArrayに適用する値のリストを受け取る
+threeTimesBiB = accum (*) updateBiB $ zip [0 .. 3] $ cycle [3]
