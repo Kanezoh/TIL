f4b0529a28472b116b1c8d87304969ca9d61c22c
Kanezoh <asfaltmanta114@gmail.com>
2021-01-04 11:11:24 +0900

    バブルソートの実装

--- haskell/chap42/uarray.hs
+++ haskell/chap42/uarray.hs
@@ -48,3 +48,22 @@ listToUArray vals = runSTUArray $ do
     let val = vals !! i
     writeArray myArray i val
   return myArray
+
+-- バブルソート
+-- 元データ
+myData :: UArray Int Int
+myData = listArray (0,5) [7,6,4,8,10,2]
+
+bubbleSort :: UArray Int Int -> UArray Int Int
+bubbleSort myArray = runSTUArray $ do
+  stArray <- thaw myArray
+  let end = (snd . bounds) myArray
+  forM_ [1 .. end] $ \i -> do
+    forM_ [0 .. (end - i)] $ \j -> do
+      val <- readArray stArray j
+      nextVal <- readArray stArray (j + 1)
+      let outOfOrder = val > nextVal
+      when outOfOrder $ do
+        writeArray stArray j nextVal
+        writeArray stArray (j + 1) val
+  return stArray
