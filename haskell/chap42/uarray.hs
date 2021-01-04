da0f45e455b1bee096ab295e3957b3cba6015daf
Kanezoh <asfaltmanta114@gmail.com>
2021-01-04 10:46:04 +0900

    リストからSTUArrayへの変換

--- haskell/chap42/uarray.hs
+++ haskell/chap42/uarray.hs
@@ -1,4 +1,7 @@
 import Data.Array.Unboxed
+import Data.Array.ST
+import Control.Monad
+import Control.Monad.ST
 
 -- 第一引数: インデックスの下限と上限
 -- 第二引数: インデックスと値のペアからなるリスト
@@ -24,3 +27,14 @@ updateBiB = beansInBuckets // [(1,5), (3,6)]
 
 -- accum関数、二項関数,UArray,UArrayに適用する値のリストを受け取る
 threeTimesBiB = accum (*) updateBiB $ zip [0 .. 3] $ cycle [3]
+
+-- 値をインプレース(その場、コピー等を使用しない)で変更するにはSTUArrayが必要
+-- Int型のリストをSTUArrayに変更する
+listToSTUArray :: [Int] -> ST s (STUArray s Int Int)
+listToSTUArray vals = do
+  let end = length vals -1
+  myArray <- newArray (0,end) 0
+  forM_ [0 .. end] $ \i -> do
+    let val = vals !! 1
+    writeArray myArray i val
+  return myArray
