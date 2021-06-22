0a26a6fdefce27d035a263cd87648f3d63bc5b9f
Kanezoh <47489839+Kanezoh@users.noreply.github.com>
2021-06-22 16:22:16 +0900

    Create struct.c

---
+++ clang/struct.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+// 構造体タグ、型ではない
+struct student {
+  int year;
+  int clas;
+  int number;
+  char name[64];
+  double stature;
+  double weight;
+};
+  
+int main(void)
+{
+  struct student data1, data2;
+  strcpy(data.name, "MARIO");
+  printf("%s\n", data.name);
+  return 0;
+}
