f785f5fbb950bc14dc093b71b63fc55491cc6187
Kanezoh <47489839+Kanezoh@users.noreply.github.com>
2021-08-19 15:37:08 +0900

    Update robot.hs

--- haskell/robot.hs
+++ haskell/robot.hs
@@ -21,3 +21,20 @@ setHp aRobot newHp         = aRobot (\(n,a,h) -> robot (n, a, newHp))
 printRobot aRobot = aRobot (\(n,a,h) -> n ++ 
                                         " attack:" ++ (show a) ++
                                         " hp:" ++ (show h))
+
+-- ダメージを与える
+damage aRobot attackDamage = aRobot (\(n,a,h) -> robot (n, a, h-attackDamage))
+
+fight aRobot defender = damage defender attack
+  where attack = if getHp aRobot > 10
+                 then getAttack aRobot
+                 else 0
+
+gentleGiant = robot ("Mr Friendly", 10 , 300)
+
+gentleGiantRound1 = fight killerRobot gentleGiant
+killerRobotRound1 = fight gentleGiant killerRobot
+gentleGiantRound2 = fight killerRobotRound1 gentleGiantRound1
+killerRobotRound2 = fight gentleGiantRound1 killerRobotRound1
+gentleGiantRound3 = fight killerRobotRound2 gentleGiantRound2
+killerRobotRound3 = fight gentleGiantRound2 killerRobotRound2
