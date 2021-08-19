921357e9c304ef059534e50424207941f3070aba
Kanezoh <47489839+Kanezoh@users.noreply.github.com>
2021-08-19 15:47:51 +0900

    Update robot.hs

--- haskell/robot.hs
+++ haskell/robot.hs
@@ -21,7 +21,6 @@ setHp aRobot newHp         = aRobot (\(n,a,h) -> robot (n, a, newHp))
 printRobot aRobot = aRobot (\(n,a,h) -> n ++ 
                                         " attack:" ++ (show a) ++
                                         " hp:" ++ (show h))
-
 -- ダメージを与える
 damage aRobot attackDamage = aRobot (\(n,a,h) -> robot (n, a, h-attackDamage))
 
@@ -38,3 +37,21 @@ gentleGiantRound2 = fight killerRobotRound1 gentleGiantRound1
 killerRobotRound2 = fight gentleGiantRound1 killerRobotRound1
 gentleGiantRound3 = fight killerRobotRound2 gentleGiantRound2
 killerRobotRound3 = fight gentleGiantRound2 killerRobotRound2
+
+
+fastRobot = robot ("speedy",15,40)
+slowRobot = robot ("slowpoke",20,30)
+
+-- fastRobotRound1 = fight fastRobot slowRobot
+-- slowRobotRound1 = fight slowRobot fastRobot
+-- fastRobotRound2 = fight fastRobotRound1 slowRobotRound1
+-- slowRobotRound2 = fight slowRobotRound1 fastRobotRound1
+-- fastRobotRound3 = fight fastRobotRound2 slowRobotRound2
+-- slowRobotRound3 = fight slowRobotRound2 fastRobotRound2
+
+slowRobotRound1 = fight fastRobot slowRobot
+fastRobotRound1 = fight slowRobotRound1 fastRobot
+slowRobotRound2 = fight fastRobotRound1 slowRobotRound1
+fastRobotRound2 = fight slowRobotRound2 fastRobotRound1
+slowRobotRound3 = fight fastRobotRound2 slowRobotRound2
+fastRobotRound3 = fight slowRobotRound3 fastRobotRound2
