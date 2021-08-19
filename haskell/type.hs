2f8ea103bc49508180a5cf2366d36e50b8365e9e
Kanezoh <47489839+Kanezoh@users.noreply.github.com>
2021-08-19 17:28:55 +0900

    Update type.hs

--- haskell/type.hs
+++ haskell/type.hs
@@ -1,5 +1,27 @@
 -- 型に別名を付ける型シノニム
-type FirstName = String 
+type FirstName  = String
+type MiddleName = String 
+type LastName   = String
 
 name :: FirstName
 name = "John"
+
+-- 新しい型を作成する
+data Sex = Male | Female
+sexInitial :: Sex -> Char
+sexInitial Male = 'M'
+sexInitial Female = 'F'
+
+-- data Name = Name FirstName MiddleName LastName
+data Name = Name { firstName :: FirstName
+                 , middleName :: MiddleName
+                 , lastName :: LastName
+}
+name2 = Name "John" "Author" "Hey"
+
+name3 = Name { firstName = "firstName"
+          , middleName = "middleName"
+          , lastName = "lastName"
+}
+
+showName (Name f m l) = (show f) ++ (show m) ++ (show l)
