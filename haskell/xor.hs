24ec79deee53700fedf22ae49adb369b0e77419b
Kanezoh <47489839+Kanezoh@users.noreply.github.com>
2021-08-24 14:28:22 +0900

    Update xor.hs

--- haskell/xor.hs
+++ haskell/xor.hs
@@ -40,3 +40,24 @@ bitsToInt bits = sum (map (\x -> 2^(snd x)) trueLocations)
 
 bitsToChar :: Bits -> Char
 bitsToChar bits = toEnum (bitsToInt bits)
+
+-- ワンタイムパッド
+myPad :: String
+myPad = "Shhhhh"
+
+myPlainText :: String
+myPlainText = "Haskell"
+
+-- 文字列とパッドをビットに変換
+applyOTP' :: String -> String -> [Bits]
+applyOTP' pad plaintext = map (\pair -> (fst pair) `xor` (snd pair))
+                            (zip padBits plaintextBits)
+    where padBits = map charToBits pad
+          plaintextBits = map charToBits plaintext
+
+applyOTP :: String -> String -> String
+applyOTP pad plaintext = map bitsToChar bitList
+  where bitList = applyOTP' pad plaintext
+
+encoderDecoder :: String -> String
+encoderDecoder = applyOTP myPad
